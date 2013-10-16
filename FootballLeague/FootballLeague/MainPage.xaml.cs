7be90ea56afffc9781049382cb75b95603f77418
unknown <duytrinh@HSSC1PCL00674.HarveyNash.vn.local>
2013-10-16 17:50:32 +0700

    commit 16-10

--- FootballLeague/FootballLeague/MainPage.xaml.cs
+++ FootballLeague/FootballLeague/MainPage.xaml.cs
@@ -1,7 +1,9 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Net;
+using System.Runtime.InteropServices;
 using System.Windows;
 using System.Windows.Controls;
 using System.Windows.Navigation;
@@ -32,6 +34,32 @@ namespace FootballLeague
         private void PhoneApplicationPage_BackKeyPress_1(object sender, System.ComponentModel.CancelEventArgs e)
         {
             NavigationService.GoBack();
+        }
+        private void Teams_Tap(object sender, GestureEventArgs e)
+        {
+            NavigationService.Navigate(new Uri("/Teams.xaml" , UriKind.Relative));
+        }
+
+        private void Button_Click_1(object sender, RoutedEventArgs e)
+        {
+            FileStream readStream;
+            try
+            {
+                readStream = new FileStream("Resources/PR1.pr", FileMode.Open);
+                BinaryReader readBinary = new BinaryReader(readStream);
+                byte[] Buffer = new byte[4];
+                int offset = 0;
+                string Binastring = readBinary.ReadString();
+
+                //string hdr = Convert.ToString(readBinary.Read(Buffer, 0, 4));
+
+                MessageBox.Show(Binastring);
+                readStream.Close();
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.ToString());
+            }
         }
     }
 }

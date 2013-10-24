2492864bc57c711494e29d326eb14c7bca2d140d
unknown <duytrinh@HSSC1PCL00674.HarveyNash.vn.local>
2013-10-24 10:16:36 +0700

    comit code

--- FootballLeague/FootballLeague/MainPage.xaml.cs
+++ FootballLeague/FootballLeague/MainPage.xaml.cs
@@ -39,21 +39,41 @@ namespace FootballLeague
         {
             NavigationService.Navigate(new Uri("/Teams.xaml" , UriKind.Relative));
         }
+        private void Matchs_Tap(object sender, GestureEventArgs e)
+        {
+            NavigationService.Navigate(new Uri("/Matchs.xaml", UriKind.Relative));
+        }
+        private void History_Tap(object sender, GestureEventArgs e)
+        {
+            NavigationService.Navigate(new Uri("/History.xaml", UriKind.Relative));
+        }
+
+        public int cFileVersion = 1;
+        public int cHeaderSize = 89;
+        public int cTeamSize = 42;
+        public int cMatchSize = 9;
+        public int cLeagueNameChars = 16;
+        public int cTeamNameChars = 10;
 
         private void Button_Click_1(object sender, RoutedEventArgs e)
         {
             FileStream readStream;
             try
             {
+
                 readStream = new FileStream("Resources/PR1.pr", FileMode.Open);
                 BinaryReader readBinary = new BinaryReader(readStream);
-                byte[] Buffer = new byte[4];
+                byte[] Buffer = readBinary.ReadBytes(4);
                 int offset = 0;
-                string Binastring = readBinary.ReadString();
+                string hdr = System.Text.Encoding.UTF8.GetString(Buffer, offset, 4);
+                offset += 5;
+                byte[] Buffer2 = readBinary.ReadBytes(offset + cLeagueNameChars * 4);
+                string LeagueName = System.Text.Encoding.UTF8.GetString(Buffer2, 1, cLeagueNameChars * 4);
+                offset += cLeagueNameChars*4;
+                byte[] mYear = readBinary.ReadBytes(offset+3);
+                int Year = BitConverter.ToInt32(mYear,0);
 
-                //string hdr = Convert.ToString(readBinary.Read(Buffer, 0, 4));
-
-                MessageBox.Show(Binastring);
+                MessageBox.Show(hdr);
                 readStream.Close();
             }
             catch (Exception ex)
